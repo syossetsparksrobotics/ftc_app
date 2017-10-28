7527dec8ac1a55550520d4ea9513baaa9d3f2ca2
Joshua-PC\Joshua <joshuayun61@gmail.com>
2017-10-27 23:29:05 -0400

    Updated the encoder code to set power and created a new DC motor code for a motor called grabber. Also uses endoer for precise movements.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_control.java
@@ -11,17 +11,36 @@ public class motor_control extends OpMode {
     private DcMotor motorleft;
     private DcMotor motorright;
     private DcMotor motorvertical;
+    private DcMotor motorgrabber;
 
 
-    public void verticalup(){
+    private void verticalup(){
         motorvertical.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorvertical.setTargetPosition(1120);
         motorvertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorvertical.setPower(0.5);
+        motorvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    public void verticaldown(){
+    private void verticaldown(){
         motorvertical.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorvertical.setTargetPosition(-1120);
         motorvertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorvertical.setPower(-0.5);
+        motorvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    private void grabberclose() {
+        motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorgrabber.setTargetPosition(-1120);
+        motorgrabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorgrabber.setPower(-0.5);
+        motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    private void grabberopen() {
+        motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorgrabber.setTargetPosition(1120);
+        motorgrabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorgrabber.setPower(0.5);
+        motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -29,17 +48,26 @@ public class motor_control extends OpMode {
         motorleft = hardwareMap.dcMotor.get("motorleft");
         motorright = hardwareMap.dcMotor.get("motorright");
         motorvertical = hardwareMap.dcMotor.get("motorvertical");
+        motorgrabber = hardwareMap.dcMotor.get("motorgrabber");
         motorright.setDirection(DcMotor.Direction.REVERSE);
+        motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorvertical.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
     public void loop() {
         motorleft.setPower(gamepad1.left_stick_y);
         motorright.setPower(gamepad1.right_stick_y);
-        if(gamepad1.a){
+        while(gamepad1.left_bumper){
+            grabberclose();
+        }
+        while(gamepad1.left_bumper){
+            grabberopen();
+        }
+        if(gamepad1.y){
             verticalup();
         }
-        if(gamepad1.b){
+        if(gamepad1.a){
             verticaldown();
         }
         }
