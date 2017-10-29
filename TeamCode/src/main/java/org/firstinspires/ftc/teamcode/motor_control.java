19930123d49d3d1cc0cd5d61c2ed7f453909f5cb
Joshua-PC\Joshua <joshuayun61@gmail.com>
2017-10-29 19:21:36 -0400

    commented out grabber motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_control.java
@@ -11,7 +11,7 @@ public class motor_control extends OpMode {
     private DcMotor motorleft;
     private DcMotor motorright;
     private DcMotor motorvertical;
-    private DcMotor motorgrabber;
+    //private DcMotor motorgrabber;
 
 
     private void verticalup(){
@@ -28,7 +28,7 @@ public class motor_control extends OpMode {
         motorvertical.setPower(-0.5);
         motorvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    private void grabberclose() {
+    /*private void grabberclose() {
         motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorgrabber.setTargetPosition(-1120);
         motorgrabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -42,28 +42,30 @@ public class motor_control extends OpMode {
         motorgrabber.setPower(0.5);
         motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    */
 
     @Override
     public void init(){
         motorleft = hardwareMap.dcMotor.get("motorleft");
         motorright = hardwareMap.dcMotor.get("motorright");
         motorvertical = hardwareMap.dcMotor.get("motorvertical");
-        motorgrabber = hardwareMap.dcMotor.get("motorgrabber");
+        //motorgrabber = hardwareMap.dcMotor.get("motorgrabber");
         motorright.setDirection(DcMotor.Direction.REVERSE);
-        motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       // motorgrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorvertical.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //motorgrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
     public void loop() {
         motorleft.setPower(gamepad1.left_stick_y);
         motorright.setPower(gamepad1.right_stick_y);
-        while(gamepad1.left_bumper){
+        /*while(gamepad1.left_bumper){
             grabberclose();
         }
         while(gamepad1.left_bumper){
             grabberopen();
         }
+        */
         if(gamepad1.y){
             verticalup();
         }
